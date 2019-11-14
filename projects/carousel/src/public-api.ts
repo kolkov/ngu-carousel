a0fbce59617010d01603c86cb54e9467d044fa52
Andy <a.kolkov@gmail.com>
2019-11-14 18:56:18 +0300

    Fix all errors after updating to Angular 9.0.0-rc.2

---
+++ projects/carousel/src/public-api.ts
@@ -0,0 +1,27 @@
+/*
+ * Public API Surface of carousel
+ */
+
+export { NguCarouselConfig } from './lib/ngu-carousel/ngu-carousel';
+
+export { NguCarouselStore } from './lib/ngu-carousel/ngu-carousel';
+
+export { NguCarousel } from './lib/ngu-carousel/ngu-carousel.component';
+
+export { NguCarouselModule } from './lib/ngu-carousel.module';
+
+export { NguTileComponent } from './lib/ngu-tile/ngu-tile.component';
+
+export { NguItemComponent } from './lib/ngu-item/ngu-item.component';
+
+export { NguCarouselPointDirective } from './lib/ngu-carousel.directive';
+
+export { NguCarouselItemDirective } from './lib/ngu-carousel.directive';
+
+export { NguCarouselNextDirective } from './lib/ngu-carousel.directive';
+
+export { NguCarouselPrevDirective } from './lib/ngu-carousel.directive';
+
+export { NguCarouselDefDirective } from './lib/ngu-carousel.directive';
+
+export { NguCarouselOutlet } from './lib/ngu-carousel.directive';
